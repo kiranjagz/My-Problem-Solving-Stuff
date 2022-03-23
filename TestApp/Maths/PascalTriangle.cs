6e089f3257c43438d48a41ad43f9df79de8d7184
Kiran <kiranjagz@gmai.com>
2022-03-23 22:09:24 +0200

    feat: added pascal and upgrade to .net 4.8.

---
+++ TestApp/Maths/PascalTriangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Maths
+{
+    public class PascalTriangle
+    {
+        public int IsPascalTriangle(int col, int row)
+        {
+            if (col == 0)
+            {
+                return 1;
+            }
+            return (row * IsPascalTriangle(col - 1, row - 1)) / col;
+        }
+    }
+}
